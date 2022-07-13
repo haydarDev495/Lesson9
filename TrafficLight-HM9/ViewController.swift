c6c0468d3f8258d844390f106e5ed29128878d7f
Haydar Bekmuradov <haydarbekmuradov50@gmail.com>
2022-07-13 03:07:16 +0300

    Initial Commit

---
+++ TrafficLight-HM9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLight-HM9
+//
+//  Created by Haydar Bekmuradov on 13.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
